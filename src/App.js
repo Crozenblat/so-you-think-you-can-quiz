d6c3393751129a39395a3cc97cc3c11756c94f78
Corey Rozenblat <coreyrozenblat@protonmail.com>
2020-07-20 00:36:40 -0400

    Removing tab-based focus from mouse users

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import './App.css';
 import TriviaBox from "./containers/TriviaBox/TriviaBox";
 import BackgroundPanels from "./components/UI/BackgroundPanels/BackgroundPanels";
 
+// Let the document know when the mouse is being used
+document.body.addEventListener('mousedown', function() {
+  document.body.classList.add('using-mouse');
+});
+
+// Re-enable focus styling when Tab is pressed
+document.body.addEventListener('keydown', function(event) {
+  if (event.keyCode === 9) {
+    document.body.classList.remove('using-mouse');
+  }
+});
+
 function App() {
   const [uniqueId, setUniqueId] = useState(0);
 
